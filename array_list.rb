dc59994f40f9568d09f2b08a71c5707a6d6238ca
viKuo <lost.angel.fin@gmail.com>
2016-08-11 22:11:12 -0500

    fixed arraylist implementation

--- array_list.rb
+++ array_list.rb
@@ -4,24 +4,22 @@ class ArrayList
   def initialize(size)
     @size = size
     @array_list = FixedArray.new(size)
+    @length = 0
   end
 
   def add(element)
-    new_array_list = FixedArray.new(@size+1)
-    i = 0
-    while i < @array_list.size
-      new_array_list.set(i, @array_list.get(i))
-      i += 1
-    end
-    new_array_list.set(new_array_list.size-1, element)
-    @array_list = new_array_list
+    grow_array if full?
+    @array_list << element
+    @length += 1
   end
 
   def get(index)
+    raise IndexOutOfBoundsError if index >= self.length || index > 0
     @array_list.get(index)
   end
 
   def set(index, element)
+    raise IndexOutOfBoundsError if index >= self.length || index > 0
     @array_list.set(index, element)
   end
 
@@ -29,64 +27,15 @@ class ArrayList
     @array_list.size
   end
 
-  def fill_array
-    @array_list.fill_array
-  end
-end
-
-class ArrayList
-  attr_reader :length
-
-  def initialize(initialCapacity = 16)
-    @initialCapacity = initialCapacity
-    @array = FixedArray.new(@initialCapacity)
-    @length = 0
-  end
-
-  def add(object)
-    grow_array if fixed_array_full?
-    @array.set(length, object)
-    @length += 1
-    object
-  end
-
-  def get(index)
-    raise IndexOutOfBoundsError if index >= self.length || index < 0
-    @array.get(index)
-  end
-
-  def set(index, object)
-    raise IndexOutOfBoundsError if index >= self.length || index < 0
-    @array.set(index, object)
-  end
-
-  def insert(index, object)
-    raise IndexOutOfBoundsError if index > self.length || index < 0
-    grow_array if fixed_array_full?
-
-    shift_contents_right_from(index)
-    @array.set(index, object)
-    @length += 1
-    object
-  end
-
   private
+  def full?
+    @size == @array_list.length
+  end
 
   def grow_array
-    new_array = FixedArray.new(@initialCapacity * 2)
-    0.upto(@length - 1) do |i|
-      new_array.set(i, @array.get(i))
-    end
-    @array = new_array
-  end
-
-  def fixed_array_full?
-    @array.size == @length
-  end
-
-  def shift_contents_right_from(index)
-    (@length - 1).downto(index) do |i|
-      @array.set(i + 1, @array.get(i))
-    end
+    @size *= 2
+    old_array = @array_list
+    @array_list = FixedArray.new(@size)
+    old_array.each { |object| @array_list << object }
   end
 end
